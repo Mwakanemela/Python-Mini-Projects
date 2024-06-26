57cbff57210a1252dd34cd0b78f22e8f661e42e0
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-26 11:02:35 +0000

    information about the location that corresponds to a phone number

--- FindPhoneNumberServiceProvider/main.py
+++ FindPhoneNumberServiceProvider/main.py
@@ -1,7 +1,10 @@
 import phonenumbers
-from phonenumbers import carrier
+# from phonenumbers import carrier
 from phonenumbers import geocoder
 
 number = "+265886473493"
+# The PhoneNumber object that parse produce
 phone_number = phonenumbers.parse(number)
+
+ # information about the location that corresponds to a phone number
 print(geocoder.description_for_number(phone_number, "en"))
