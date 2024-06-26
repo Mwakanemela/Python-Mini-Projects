ea89e630a3e9bbdf167b5d7ca8224829155cce46
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-26 11:08:44 +0000

    Getting phonenumber : Location,  Time zone and Service Provider

--- FindPhoneNumberServiceProvider/main.py
+++ FindPhoneNumberServiceProvider/main.py
@@ -1,10 +1,13 @@
 import phonenumbers
-# from phonenumbers import carrier
+from phonenumbers import carrier
 from phonenumbers import geocoder
+from phonenumbers import timezone
 
 number = "+265886473493"
 # The PhoneNumber object that parse produce
 phone_number = phonenumbers.parse(number)
 
  # information about the location that corresponds to a phone number
-print(geocoder.description_for_number(phone_number, "en"))
+print("Location        : ",geocoder.description_for_number(phone_number, "en"))
+print("Time zone       : ", timezone.time_zones_for_number(phone_number))
+print("Service Provider: ",carrier.name_for_number(phone_number, "en"))
